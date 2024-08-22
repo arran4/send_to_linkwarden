7778de66d7f6b12a213685a0f923a54d7391fd97
Arran Ubels <arran4@gmail.com>
2024-08-22 17:13:13 +1000

    More enhancements

--- lib/core/keyed_pub_sub_replay.dart
+++ lib/core/keyed_pub_sub_replay.dart
@@ -1,10 +1,11 @@
 import 'dart:async';
+import 'dart:math';
 
 class KeyedPubSubReplay<K, T> {
   final Map<K, T> _lastMessage = <K, T>{};
   K _currentKey;
   final List<StreamController<T>> _subscribers = [];
-  void Function(KeyedPubSubReplay<K, T> queue, T currentKey)? _onNoLastMessage;
+  final void Function(KeyedPubSubReplay<K, T> queue, T currentKey)? _onNoLastMessage;
 
   KeyedPubSubReplay({required K currentKey, void Function(KeyedPubSubReplay<K, T> queue, T currentKey)? onNoLastMessage})
       : _onNoLastMessage = onNoLastMessage, _currentKey = currentKey;
@@ -18,20 +19,30 @@ class KeyedPubSubReplay<K, T> {
     _checkAndInitialize();
   }
 
-  void _checkAndInitialize() {
+  void _checkAndInitialize({ StreamController<T>? singleTarget }) {
     if (_lastMessage.containsKey(currentKey)) {
-      publish(_lastMessage[currentKey] as T, keyCheck: currentKey);
+      publish(_lastMessage[currentKey] as T, keyCheck: currentKey, singleTarget: singleTarget);
     } else {
       _onNoLastMessage?.call(this, currentKey);
     }
   }
 
-  void publish(T message, { K? keyCheck}) {
+  void reset(K key) {
+    _lastMessage.remove(key);
+    if (currentKey == key) {
+      _checkAndInitialize();
+    }
+  }
+
+  void publish(T message, { K? keyCheck, StreamController<T>? singleTarget }) {
     if (keyCheck != null && currentKey != keyCheck) {
       _lastMessage[keyCheck] = message;
       return;
     }
     _lastMessage[currentKey] = message;
+    if (singleTarget != null) {
+      singleTarget.add(message);
+    }
     for (var subscriber in _subscribers) {
       subscriber.add(message);
     }
@@ -40,7 +51,7 @@ class KeyedPubSubReplay<K, T> {
   Stream<T> subscribe() {
     StreamController<T> controller = StreamController<T>.broadcast();
     controller.onListen = () {
-      _checkAndInitialize();
+      _checkAndInitialize(singleTarget: controller);
     };
     _subscribers.add(controller);
 
